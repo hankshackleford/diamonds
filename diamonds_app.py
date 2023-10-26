e4ad60d9cb692f861c54a09c1e4ef7ea8a1335b6
hankshackleford <143135567+hankshackleford@users.noreply.github.com>
2023-10-26 11:15:11 -0400

    Update diamonds_app.py

--- diamonds_app.py
+++ diamonds_app.py
@@ -5,21 +5,26 @@ Created on Thu Oct 26 10:41:08 2023
 @author: edwar
 """
 
-#Import packages
-import pandas as pd
-import pygwalker as pyg
 from pygwalker.api.streamlit import StreamlitRenderer, init_streamlit_comm
+import pandas as pd
 import streamlit as st
-
-#Set titles and sidebar header
-st.set_page_config(page_title="Diamonds", page_icon="💎", layout="wide")
-st.title('Diamonds')
-
-
+ 
+# Adjust the width of the Streamlit page
+st.set_page_config(
+    page_title="Use Pygwalker In Streamlit",
+    layout="wide"
+)
+ 
+# Establish communication between pygwalker and streamlit
+init_streamlit_comm()
+ 
+# Add a title
+st.title("Use Pygwalker In Streamlit")
+ 
 # Get an instance of pygwalker's renderer. You should cache this instance to effectively prevent the growth of in-process memory.
 @st.cache_resource
 def get_pyg_renderer() -> "StreamlitRenderer":
-    df = pd.read_csv("https://raw.githubusercontent.com/tidyverse/ggplot2/main/data-raw/diamonds.csv")
+    df = pd.read_csv("https://kanaries-app.s3.ap-northeast-1.amazonaws.com/public-datasets/bike_sharing_dc.csv")
     # When you need to publish your app to the public, you should set the debug parameter to False to prevent other users from writing to your chart configuration file.
     return StreamlitRenderer(df, spec="./gw_config.json", debug=False)
  
